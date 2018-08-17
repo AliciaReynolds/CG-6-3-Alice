68fadf4308ed8be6d6359b2e239dff32fddd1bc5
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-16 23:46:48 -0500

    Code written

--- CG 6-3 Alice/Program.cs
+++ CG 6-3 Alice/Program.cs
@@ -6,18 +6,25 @@ namespace CG_6_3_Alice
     {
         static void Main(string[] args)
         {
-            string Alice = "Alice was beginning to get very tired of sitting by her sister/n " +
-                "on the bank, and of having nothing to do: once or twice she had peeped into/n" +
-                " the book her sister was reading, but it had no pictures or conversations in it,/n" +
-                " ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
+            string Alice = "\nAlice was beginning to get very tired of sitting by her sister\n" +
+                "on the bank, and of having nothing to do: once or twice she had peeped into\n" +
+                "the book her sister was reading, but it had no pictures or conversations in it,\n" +
+                "‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
 
             Console.WriteLine($"Provide a word and see if it exists in the quote, {Alice}.");
+            Console.WriteLine();
 
-            var search = Alice.IndexOf(Console.ReadLine());
-            string AliceSearch = Convert.ToString(search);
+            var search = Console.ReadLine();
+            var start = Alice.IndexOf(search);
+            var end = search.Length;
+            //string AliceSearch = Convert.ToString(search);
 
             
-            if (Alice.Contains(AliceSearch))
+
+            
+            //if (Alice.Contains(AliceSearch))
+            if (Alice.Contains(search))
+
             {
                 Console.WriteLine("True. The word was found within the quote.");
             }
