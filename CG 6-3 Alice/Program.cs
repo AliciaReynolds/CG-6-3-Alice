1ca25079831a3900fc2565ed493512cccdbc81a5
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-18 12:34:30 -0500

    more trying

--- CG 6-3 Alice/Program.cs
+++ CG 6-3 Alice/Program.cs
@@ -20,18 +20,29 @@ namespace CG_6_3_Alice
             var start = Alice.IndexOf(search);
             var end = search.Length;
 
-            
+            //Contains(search);
             //string title = search;
             //bool contains = Alice.Contains("title", StringComparison.OrdinalIgnoreCase);
             //^^That doesn't seem to work.  "Contains" says an overload issue that I can't 
             //figure out how to fix
+
+            /*vvv thought this was a way to do CI -- no go
+           StringCommparison comp = StringComparison.Ordinal;
+            Console.WriteLine("        {0;G}:   {1]", comp, Alice.Contains(search, comp));
+
+            comp = StringComparison.OrdinalIgnoreCase;
+            Console.WriteLine("   {0:G}:  {1}", comp, Alice.Contains(search, comp));*/
 
 
 
             //if (Alice.Contains(AliceSearch))
             if (Alice.Contains(search)) //<--- this one works but is case sensitive
             //if (Alice.ToUpper().Contains(SEARCH)) //<---tried this for case insensitive.  No go.
-
+            //if(Alice.Equals(search, StringComparison.InvariantCultureIgnoreCase)) <--tried this for case insensitive. No go.
+            //if (string.Equals(Alice, search, StringComparison.OrdinalIgnoreCase)) <-- tried this for caase insensitive. No go.
+            //if (Alice.Contains(search, StringComparison.OrdinalIgnoreCase)) <--tried this to make case insensitive. No go.
+            //if (search.ToLowerCase() == Console.ReadLine()) <--No go for CI (case insensitive)
+            //if (Alice.ToLowerCase() == search) <--No go for CI
             {
                 Console.WriteLine("True. The word was found within the quote.");
             }
@@ -48,10 +59,16 @@ namespace CG_6_3_Alice
             Console.ReadLine();
 
         }
+        //vvv all that was trying to create methods to make it case insensitive
+        //private static void Contains(string search)
+        //{
+        //    throw new NotImplementedException();
+        //}this was the sugggested fix to the bool method to try and make it not case insenstive
 
-        public static bool Contains (this string source, string toCheck, StringComparison comp)
-        {
-            return source.IndexOf(toCheck, comp) >= 0;
-        }
+
+        //public static bool Contains (this string source, string toCheck, StringComparison comp)
+        //{
+        //    return source.IndexOf(toCheck, comp) >= 0;
+        //}this was a way to make it case insenstive. Did not work
     }
 }
